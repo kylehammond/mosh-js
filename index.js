12d5ac2c1d0d211c4d6e62eb9da0790e8a72c071
Kyle Hammond <Kyle.Hammond@concordiaplans.org>
2021-12-30 14:37:48 -0600

    switch / case

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-// if else
+// switch / case
 
-// if hour is between 6am and 12pm:  Good morning!
-// if hour is between 12pm and 6pm:  Good afternoon!
-// otherwise:  Good evening!
+let role;
+role = "moderator";
 
-let hour = 24;
-
-if (hour >= 6 && hour < 12) {
-  console.log("Good morning!");
-} else if (hour >= 12 && hour < 18) {
-  console.log("Good afternoon");
-} else {
-  console.log("Good evening!");
+switch (role) {
+  case "guest":
+    console.log("Guest User");
+    break;
+  case "moderator":
+    console.log("Moderator User");
+    break;
+  default:
+    console.log("Unknown User");
+    break;
 }
