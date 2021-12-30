07c2fd07c075271f2837ec58c26c3d807ff7a60a
Kyle Hammond <Kyle.Hammond@concordiaplans.org>
2021-12-30 11:45:37 -0600

    constants

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
-// let name;
-// console.log(name); // undefined
+// let interestRate = 0.3;
+// interestRate = 1;
+// console.log(interestRate);
 
-let name = 'Kyle';
-console.log(name); 
+const interestRate = 0.3;
+interestRate = 1; //will throw TypeError: Assignment to constant variable 
+console.log(interestRate);
 
-// variable naming rules
 
-// Cannot be reserved keyword
-// Should be meaningful
-// Cannot start with a number (1name)
-// Cannot contain space or hyphen (-)
-// Are case-sensitive  (firstName diff from FirstName)
-
-// conventions
-// camelCase
-
-// not best practice
-// let firstName = 'Kyle', lastName = 'Hammond'; 
-
-// best practice
-// let firstName = 'Kyle';
-// let lastName = 'Hammond';
+// const is default until you need to reassign
