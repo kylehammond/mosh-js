7f39909e058da36ead35d5814c1531ad6bbaa71c
Kyle Hammond <Kyle.Hammond@concordiaplans.org>
2021-12-30 11:43:59 -0600

    variables and conventions

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-// comment
-console.log('Hello world');
+// let name;
+// console.log(name); // undefined
+
+let name = 'Kyle';
+console.log(name); 
+
+// variable naming rules
+
+// Cannot be reserved keyword
+// Should be meaningful
+// Cannot start with a number (1name)
+// Cannot contain space or hyphen (-)
+// Are case-sensitive  (firstName diff from FirstName)
+
+// conventions
+// camelCase
+
+// not best practice
+// let firstName = 'Kyle', lastName = 'Hammond'; 
+
+// best practice
+// let firstName = 'Kyle';
+// let lastName = 'Hammond';
