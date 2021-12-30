abe954b1a72c0baee24273f594ac50a2eedef212
Kyle Hammond <Kyle.Hammond@concordiaplans.org>
2021-12-30 14:56:02 -0600

    loops

--- index.js
+++ index.js
@@ -1,16 +1,55 @@
-// switch / case
+// Loops
 
-let role;
-role = "moderator";
+// for
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
 
-switch (role) {
-  case "guest":
-    console.log("Guest User");
-    break;
-  case "moderator":
-    console.log("Moderator User");
-    break;
-  default:
-    console.log("Unknown User");
-    break;
+// while
+let j = 0;
+while (j < 10) {
+  console.log(j++);
+}
+
+// do while
+let k = 0;
+do {
+  console.log(k++);
+} while (k < 10);
+
+// infinite loops
+// loops that never stop looping .. I skipped this part
+
+// for-in (iterate over properties of array))
+const person = {
+  name: "Kyle",
+  age: 35,
+};
+
+for (let key in person) {
+  console.log("Key: " + key, "Value: " + person[key]);
+}
+
+const colors = ["red", "green", "blue"];
+
+for (let index in colors) {
+  console.log(colors[index]);
+}
+
+// for-of (ES6 preferred) (iterate over the ITEMS of an array)
+for (let color of colors) {
+  console.log(color);
+}
+
+// break and continue
+let l = 0;
+while (l <= 10) {
+  // if (l === 5) break;
+  if (l % 2 === 0) {
+    l++;
+    continue;
+  }
+
+  console.log(l);
+  l++;
 }
