305fedece4af76b35522a90a63160e1fa83d6187
Kyle Hammond <Kyle.Hammond@concordiaplans.org>
2021-12-30 13:29:17 -0600

    assignment operators

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
 // Operators
 
 // Arithmetic
-let x = 10;
-let y = 3;
+// ======================================================
+// let x = 10;
+// let y = 3;
 
-console.log(x + y); // log an expression (something that produces a value)
-console.log(x - y);
-console.log(x * y);
-console.log(x / y);
-console.log(x % y);
-console.log(x ** y); // exponent, somewhat new?
+// console.log(x + y); // log an expression (something that produces a value)
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+// console.log(x ** y); // exponent, somewhat new?
 
-console.log(++x); // increment before logging
+// console.log(++x); // increment before logging
 
-console.log(x++); // increment after logging
-console.log(x); // increment after logging
+// console.log(x++); // increment after logging
+// console.log(x); // increment after logging
 
-console.log(--y); // decrement before logging
+// console.log(--y); // decrement before logging
 
-console.log(y--); // decrement after logging
-console.log(y); // decrement after logging
+// console.log(y--); // decrement after logging
+// console.log(y); // decrement after logging
 
 // Assignment
+// ======================================================
+let x = 10;
+
+// both same
+x = x + 5;
+x += 5;
+
+// both same
+x = x * 3;
+x *= 3;
 
 // Comparison
+// ======================================================
 
 // Logical
+// ======================================================
 
 // Bitwise
+// ======================================================
