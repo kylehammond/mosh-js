d1b34a62b579e736d4d38523e4b10240b4e3a3f4
Kyle Hammond <kyle.hammond@concordiaplans.org>
2022-06-21 16:14:11 -0500

    factory functions pt 2

--- index.js
+++ index.js
@@ -2,16 +2,15 @@
 
 function createCircle(radius) {
   return {
-    radius, //equivalent to radius: radius,
+    radius,
     draw() {
       console.log("draw");
     },
-    // equivalent
-    // draw: function () {
-    //   console.log("draw");
-    // },
   };
 }
 
 const circle1 = createCircle(1);
 console.log(circle1);
+
+const circle2 = createCircle(2);
+console.log(circle2);
