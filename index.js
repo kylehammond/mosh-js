c4c56316563600fd39612048f6a274000bc7ecd0
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-02 14:21:40 -0500

    mapping an array

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const numbers = [1, 2, -1, 3];
 
 const filteredNumbers = numbers.filter((n) => n >= 0);
 
-console.log(filteredNumbers); // array of 1,2,3
+const items = filteredNumbers.map((n) => "<li>" + n + "</li>");
+
+console.log("<ul>" + items.join("") + "</ul>"); //<li>1</li><li>2</li><li>3</li>
