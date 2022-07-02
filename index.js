8997a6d935833e42851d3c4d1272ed01393a5fc8
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-02 11:15:15 -0500

    Date object

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
-// const message = 'This is my\n' 
-//               + 'first message';
+const now = new Date();
+const date1 = new Date('May 11 2018 09:00');
+const date2 = new Date(2022,0,18,9,0); // 0 for jan
 
+console.log(now);
+console.log(date1);
+console.log(date2);
 
-// // JS literals              
-// // Object {}
-// // Boolean true, false
-// // String '', ""
-// // Template ``
+now.setFullYear(2017);
+console.log(now);
 
+console.log(now.toDateString()); // Sun Jul 02 2017
+console.log(now.toTimeString()); // 11:14:39 GMT-0500 (Central Daylight Time)
+console.log(now.toISOString()); // commonly used on back ends
 
-// const clearerMessage = 
-// `This is my
-// first message`
-
-// console.log(message);  
-// console.log(clearerMessage);
-
-// // same result
-
-const name = 'John';
-
-const email =
-`Hi ${name},
-
-Thank you for joining my mailing list.
-
-Regards,
-Kyle`
-
-console.log(email);
