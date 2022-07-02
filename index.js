1e14f2018be641b49d105f3d65226d60835c5d71
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-02 14:18:51 -0500

    filtering an array

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 const numbers = [1, 2, -1, 3];
 
-const allPositive = numbers.every(function (value) {
-  return value >= 0;
-});
+const filteredNumbers = numbers.filter((n) => n >= 0);
 
-const atLeastOnePositive = numbers.some(function (value) {
-  return value >= 0;
-});
-
-// could do arrow funcs
-// const allPositive = numbers.every((value) => value >= 0);
-
-console.log(allPositive);
-console.log(atLeastOnePositive);
+console.log(filteredNumbers); // array of 1,2,3
