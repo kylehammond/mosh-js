789eafa1c656b322338070fc39723679884419c8
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-02 10:55:25 -0500

    garbage collection

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-const circle = {
-  radius: 1,
-  draw() {
-    console.log('draw');
-  }
-};
+let circle = {}; // mem auto allocated
+console.log(circle);  // done using, no need to deallocate memory - GC'd
 
-// const another = {};
-
-// for (let key in circle){
-//   another[key] = circle[key];  // another['radius'] = circle['radius']; and so on
-// }
-
-// console.log(another); // radius: 1, draw: draw()...
-
-
-
-// const another = Object.assign({},circle);
-// console.log(another); // radius: 1, draw: draw()...
-
-
-
-// const another = Object.assign({
-//   color:'yellow'
-// },circle);
-// console.log(another); // color: "yellow", radius: 1, draw: draw()...
-
-const another = { ...circle}; //spread operator 
-console.log(another); // radius: 1, draw: draw()...
