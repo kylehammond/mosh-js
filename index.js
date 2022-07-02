af0e2cd982a67c3e9544a261b2197bfde2cfdc9e
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-02 12:45:00 -0500

    emptying an array

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-const numbers = [1, 2, 3, 4];
+// solution 1
+let numbers1 = [1, 2, 3, 4];
+let another = numbers1;
 
-//push, unshift, splice for adding
-//pop, shift, splice for removing
+numbers1 = []; // would be GC'd normally but not if another ref to it
 
-//End
-console.log(numbers.pop()); //4
+console.log(numbers1); // empty array
+console.log(another); // still points to original reference - 1,2,3,4
 
-//Beginning
-console.log(numbers.shift()); //1
+// solution 2
+let numbers2 = [1, 2, 3, 4];
 
-//Middle
-const numbers2 = [1, 2, 3, 4];
-console.log(numbers2.splice(2, 2)); // array of 3, 4 because they were removed
+numbers2.length = 0; // preferred
+
+console.log(numbers2); // empty array
+
+// solution 3
+let numbers3 = [1, 2, 3, 4];
+
+numbers3.splice(0, numbers3.length);
+
+console.log(numbers3); // empty array
+
+// solution 4
+let numbers4 = [1, 2, 3, 4];
+
+while (numbers4.length > 0) numbers4.pop(); // not recommended - bad perf
+
+console.log(numbers4); // empty array
