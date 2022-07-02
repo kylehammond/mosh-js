b455919a15fec6a111cc3d1cda328d95a4c66e63
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-02 13:09:16 -0500

    sorting arrays

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-const numbers = [1, 2, 3];
+// const numbers = [2, 3, 1];
 
-const joined = numbers.join(",");
-console.log(joined); //1,2,3
+// console.log(numbers.sort()); // array 1,2,3
+// console.log(numbers.reverse()); // array 3,2,1
 
-const message = "this is my first message";
-const messageParts = message.split(" ");
-console.log(messageParts); // array of these words
+const courses = [
+  { id: 1, name: "Node.js" },
+  { id: 2, name: "Javascript" },
+];
 
-const combinedMessage = messageParts.join("-");
-console.log(combinedMessage); // this-is-my-first-message  (useful for url slugs)
+console.log(courses.sort()); // no change
 
-console.log(joined.split(",")); // array of "1", "2", "3"
+console.log(
+  courses.sort((a, b) => {
+    // a < b => -1
+    // a > b => 1
+    // a === b => 0
+    const nameA = a.name.toUpperCase();
+    const nameB = b.name.toUpperCase();
+
+    if (nameA < nameB) return -1;
+    if (nameA > nameB) return 1;
+    return 0;
+  })
+); // CASE MATTERS HERE ascii values... "javascript" will come after "Node.js" but "Javascript" won't
+// he fixed this with the touppercase
