0ec502da62f97fd520d126195fe0ef93a4d47a49
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-02 14:35:36 -0500

    reducing an array

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const numbers = [1, 2, -1, 3];
 
-const filteredNumbers = numbers.filter((n) => n >= 0);
+// let sum = 0;
+// for (let n of numbers) sum += n;
 
-const items = filteredNumbers.map((n) => "<li>" + n + "</li>");
+// console.log(sum);
 
-console.log("<ul>" + items.join("") + "</ul>"); //<li>1</li><li>2</li><li>3</li>
+// for each item in the array, do the callback function
+// let sum = numbers.reduce(
+//   (accumulator, currentValue) => accumulator + currentValue,
+//   0 //init value
+// );
+// // accumulator = 0, currentValue = 1 => a = 1
+// // a = 1, c = -1 => a = 0
+// // a = 0, c = 2 => a = 2
+// // a = 2, c = 3 => a = 5
+
+// for each item in the array, do the callback function
+const sum = numbers.reduce(
+  (accumulator, currentValue) => accumulator + currentValue
+);
+// a = 1, c = -1 => a = 0
+// a = 0, c = 2 => a = 2
+// a = 2, c = 3 => a = 5
+console.log(sum);
