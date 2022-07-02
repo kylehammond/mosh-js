c3addb36505508ff495cbf05deae828a61e0c7a6
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-02 10:58:49 -0500

    Math object

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-let circle = {}; // mem auto allocated
-console.log(circle);  // done using, no need to deallocate memory - GC'd
+//Mozilla docs for Math
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+
+// Math is built in Object + functionality for Math ops
+console.log(Math.random());
+console.log(Math.round(1.9));
+console.log(Math.min(1, 2, 3, 4, 5));
+console.log(Math.max(1, 2, 3, 4, 5));
 
