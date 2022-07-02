3b5ca8941d6e93b9f6e4e903ce2a8c8c16828349
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-02 12:39:55 -0500

    removing elements

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-const courses = [
-  { id: 1, name: "a" },
-  { id: 2, name: "b" },
-];
+const numbers = [1, 2, 3, 4];
 
-console.log(courses.includes({ id: 1, name: "a" })); // false - b/c ref is not same
+//push, unshift, splice for adding
+//pop, shift, splice for removing
 
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+//End
+console.log(numbers.pop()); //4
 
-// function(course) is callback function
-console.log(
-  courses.find(function (course) {
-    return course.name === "a";
-  })
-); // returns the object
+//Beginning
+console.log(numbers.shift()); //1
 
-// arrow function version of same thing (using predicate)
-console.log(courses.find((course) => course.name === "a")); // returns the object with id of 1
+//Middle
+const numbers2 = [1, 2, 3, 4];
+console.log(numbers2.splice(2, 2)); // array of 3, 4 because they were removed
