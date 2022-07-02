a0466724f45b7a0e749541c65c4e34067661a2ce
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-02 12:36:41 -0500

    arrow functions

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const courses = [
 console.log(courses.includes({ id: 1, name: "a" })); // false - b/c ref is not same
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
-console.log(courses.find((c) => c.id === 1)); // returns the object with id of 1
 
 // function(course) is callback function
 console.log(
@@ -15,20 +14,5 @@ console.log(
   })
 ); // returns the object
 
-console.log(
-  courses.find(function (course) {
-    return course.name === "xyz";
-  })
-); // undefined
-
-console.log(
-  courses.findIndex(function (course) {
-    return course.name === "a";
-  })
-); // 0
-
-console.log(
-  courses.findIndex(function (course) {
-    return course.name === "xyz";
-  })
-); // -1
+// arrow function version of same thing (using predicate)
+console.log(courses.find((course) => course.name === "a")); // returns the object with id of 1
