7b3ac51e13b1d39508cc3207170976823519505f
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-02 11:07:07 -0500

    String object / methods

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-//Mozilla docs for Math
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+// mozilla string docs
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
 
-// Math is built in Object + functionality for Math ops
-console.log(Math.random());
-console.log(Math.round(1.9));
-console.log(Math.min(1, 2, 3, 4, 5));
-console.log(Math.max(1, 2, 3, 4, 5));
+// String primitive
+const message = 'This is my first message';
 
+// String object
+const another = new String('This is my second message');
+
+console.log(typeof message);  //string
+console.log(typeof another);  //object
+
+console.log(message.length); // 24
+console.log(another.length); // 25
+
+console.log(message[0]); //T
+console.log(another[0]); //T
+console.log(message[1]); //h
+
+console.log(message.includes('my')); //true
+console.log(message.startsWith('This')); //true
+console.log(message.startsWith('this')); //false, case sensitive
+
+console.log(message.endsWith('e')); //true
+
+console.log(message.indexOf('my')); //8
+console.log(message.replace('first','third')); // returns NEW string, does not modify original - This is my third message
+
+console.log(message.toUpperCase); //THIS IS MY FIRST MESSAGE
+console.log(message.toLowerCase); //this is my first message
+console.log(message.trim); //trims..
+
+
+//escape characters are a thing, too
+
+console.log(message.split(' ')); // array of words
