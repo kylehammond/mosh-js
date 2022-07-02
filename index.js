400d8c123a687aa3c0d195e45289ac5e843d85fd
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-02 11:38:08 -0500

    Objects 16 - Exercise 2 - Factory and Constructor Functions

--- index.js
+++ index.js
@@ -1,33 +1,60 @@
-// make a new object
-// street
-// city
-// zipCode
-// showAddress(address) show all properties and their value
+// let address = {
+//   street: '123 Sesame Street',
+//   city: 'Bumfeck',
+//   state: 'WY',
+//   zipCode: '54321'
+// }
+
+// initialize an address object using a factory function and then using a constructor function
 
 // my solution
-let address = {
-  street: '123 Sesame Street',
-  city: 'Bumfeck',
-  state: 'WY',
-  zipCode: '54321',
-  showAddress() {
-      console.log(
-      `${this.street} ${this.city}, ${this.state} ${this.zipCode}`);
+
+// factory
+function createAddress(street, city, state, zipCode){
+  return {
+    address: {
+      street,
+      city,
+      state,
+      zipCode
+    }
   }
 }
 
-address.showAddress();
+const factoryAddress = createAddress('123 Sesame Street','Bumfeck','WY','54321');
+console.log(factoryAddress);
 
-// his solution
-let address2 = {
-  street: 'a',
-  city: 'b',
-  zipCode: 'c'
+// constructor
+function Address(street, city, state, zipCode){
+  this.street = street;
+  this.city = city;
+  this.state = state;
+  this.zipCode = zipCode;
 }
 
-function showAddress(address2) {
-  for (let key in address2)
-    console.log(key, address2[key]);
+const constructedAddress = new Address('123 Sesame Street','Bumfeck','WY','54321');
+console.log(constructedAddress);
+
+// factory
+function createAddress(street, city, state, zipCode){
+  return {
+    street,
+    city,
+    state,
+    zipCode
+  }
 }
 
-showAddress(address2);
+const factoryAddress = createAddress('123 Sesame Street','Bumfeck','WY','54321');
+console.log(factoryAddress);
+
+// constructor
+function Address(street, city, state, zipCode){
+  this.street = street;
+  this.city = city;
+  this.state = state;
+  this.zipCode = zipCode;
+}
+
+const constructedAddress = new Address('123 Sesame Street','Bumfeck','WY','54321');
+console.log(constructedAddress);
