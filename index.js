f3bb8b6a6ceff122f9238813d64c18f6bb828863
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-02 11:11:19 -0500

    Template literals

--- index.js
+++ index.js
@@ -1,36 +1,31 @@
-// mozilla string docs
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
-
-// String primitive
-const message = 'This is my first message';
-
-// String object
-const another = new String('This is my second message');
-
-console.log(typeof message);  //string
-console.log(typeof another);  //object
-
-console.log(message.length); // 24
-console.log(another.length); // 25
-
-console.log(message[0]); //T
-console.log(another[0]); //T
-console.log(message[1]); //h
-
-console.log(message.includes('my')); //true
-console.log(message.startsWith('This')); //true
-console.log(message.startsWith('this')); //false, case sensitive
-
-console.log(message.endsWith('e')); //true
-
-console.log(message.indexOf('my')); //8
-console.log(message.replace('first','third')); // returns NEW string, does not modify original - This is my third message
-
-console.log(message.toUpperCase); //THIS IS MY FIRST MESSAGE
-console.log(message.toLowerCase); //this is my first message
-console.log(message.trim); //trims..
+// const message = 'This is my\n' 
+//               + 'first message';
 
 
-//escape characters are a thing, too
+// // JS literals              
+// // Object {}
+// // Boolean true, false
+// // String '', ""
+// // Template ``
 
-console.log(message.split(' ')); // array of words
+
+// const clearerMessage = 
+// `This is my
+// first message`
+
+// console.log(message);  
+// console.log(clearerMessage);
+
+// // same result
+
+const name = 'John';
+
+const email =
+`Hi ${name},
+
+Thank you for joining my mailing list.
+
+Regards,
+Kyle`
+
+console.log(email);
