793a91db378fd090c644908aaa2ca712a89bb7f2
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-03 10:53:23 -0500

    rest operator

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
-// function sum(a, b) {
-//   // console.log(arguments); // returns a new object of parameters
-//   return a + b;
-// }
-
-// console.log(sum(1, 2)); // 3
-// console.log(sum(1)); // NaN - not a number  ( 1 + undefined )
-
-// console.log(sum(1, 2, 3, 4, 5)); // 3
-// // how would we do this?
-
-function sum() {
-  let total = 0;
-  for (let value of arguments) {
-    total += value;
-  }
-  return total;
+//rest operator
+function sum(discount, ...prices) {
+  // console.log(args); // array of args - if you didn't have ... it would just show "1"
+  const total = prices.reduce((a, b) => a + b);
+  return total * (1 - discount);
 }
 
-console.log(sum(1, 2)); // 3
-console.log(sum(1)); // NaN - not a number  ( 1 + undefined )
+console.log(sum(0.1, 20, 30));
 
-console.log(sum(1, 2, 3, 4, 5)); // 3
-// how would we do this?
+// rest param must be last formal parameter
+// the "rest" of the args
