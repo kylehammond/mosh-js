276ef795e96ead66dd82c82799fadd8acf7b10f5
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-03 16:56:22 -0500

    enumerating properties

--- js-oop/index.js
+++ js-oop/index.js
@@ -6,18 +6,16 @@ function Circle(radius) {
 }
 
 const circle = new Circle(10);
-circle.location = { x: 1 }; //same
-circle["location"] = { x: 1 }; //same, but useful for dynamically accessing property name
 
-const propertyName = "location";
-console.log(circle[propertyName]); // dynamic access
+for (let key in circle) {
+  console.log(key); // radius draw
+}
 
-// can also use for properties with bad names
-const otherPropertyWithBadChars = "center-location property";
+for (let key in circle) {
+  if (typeof circle[key] !== "function") console.log(key, circle[key]); // only radius
+}
 
-// you can remove properties that you don't want sent to the client such as password, etc
-delete circle.location;
-delete circle[otherPropertyWithBadChars];
+const keys = Object.keys(circle);
+console.log(keys); // array ["radius","draw"]
 
-// ex of use:
-// user.token = 'sadfasdf';
+if ("radius" in circle) console.log("Circle has a radius"); // circle has..
