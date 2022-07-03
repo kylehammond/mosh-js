eed19e5001078880691389c744792393f7aaea11
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-03 11:42:55 -0500

    changing 'this'

--- index.js
+++ index.js
@@ -7,39 +7,37 @@
 const video = {
   title: "a",
   tags: ["a", "b", "c"],
-  // showTags() {
-  //   this.tags.forEach((tag) => console.log(this, tag)); // this function is an anon callback and points to Window!!
-  // },
   showTags() {
-    this.tags.forEach((tag) => console.log(this, tag), this); // this puts you in the context of the video again.  not all methods in JS allow passing in this context.
+    // non preferred approach
+    // const self = this; // some use 'that'
+    // this.tags.forEach(function (tag) {
+    //   return console.log(self.title, tag);
+    // }); // this puts you in the context of the video again.  not all methods in JS allow passing in this context.
+
+    // second solution
+    // this.tags.forEach(
+    //   function (tag) {
+    //     console.log(this.title, tag);
+    //   }.bind(this)
+    // ); // this puts you in the context of the video again.  not all methods in JS allow passing in this context.
+
+    // new ES6 preffered way
+    this.tags.forEach((tag) => console.log(this.title, tag)); // always points to what you used
   },
-  // method (because in obj, this references this obj)
   play() {
     console.log(this);
   },
 };
 
-// // method - references video
-// video.stop = function () {
-//   console.log(this);
-// };
-
-video.play();
-// video.stop();
-
-// functions reference Window
-
-function playVideo() {
-  console.log(this);
-}
-playVideo(); // logs Window object
-
-function Video(title) {
-  this.title = title;
-  console.log(this);
-}
-
-const v = new Video("a"); // logs Video object
-
 video.showTags();
-// a  b  c  listed
+
+// function playVideo(a, b) {
+//   console.log(this);
+// }
+
+// playVideo.call({ name: "Mosh" }, 1, 2); // ref specified obj
+// playVideo.apply({ name: "Mosh" }, [1, 2]); // ref specified obj
+// playVideo.bind({ name: "Mosh" })(); // gives you back new function pointing to your obj
+// // () at the end calls it .. just like .call()
+
+// playVideo(); // ref Window
