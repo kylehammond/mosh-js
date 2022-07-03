00725f3c5652e847fe6f38c53ebcb7d88aac483a
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-03 14:32:18 -0500

    js-fundamentals wrapup

---
+++ js-fundamentals/index.js
@@ -0,0 +1,5 @@
+//This training is complete.
+
+// I did each section as a commit to the main file to keep following along with the videos simpler.  To see each lesson you need to look at the new version of the code for each commit.
+
+// I will move on to the next lesson now which is a new course about OOP in JS
