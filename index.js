1c5e64304dcf7763c9cfe1750f8d1e35a878ca3e
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-03 10:41:35 -0500

    Function declarations vs expressions

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
-// write code to get all the movies in 2018 with rating > 4
-// sort by their rating in desc order
-// only pick the title property and display it on the console:
-// expected result:
-// 'b'
-// 'a'
+// Function Declaration
+function walk() {
+  console.log("walk");
+}
 
-const movies = [
-  { title: "a", year: 2018, rating: 4.5 },
-  { title: "b", year: 2018, rating: 4.7 },
-  { title: "c", year: 2018, rating: 3 },
-  { title: "d", year: 2017, rating: 4.5 },
-];
+// Anonymous Function Expression
+let run = function () {
+  console.log("run");
+};
 
-// my solution
-console.log(
-  movies
-    .filter((m) => m.rating > 4 && m.year === 2018)
-    .reverse((m) => m.rating) // apparently needed comparison
-    .map((m) => m.title)
-);
+// Named Function Expression
+let runNamed = function runNamed() {
+  console.log("run named");
+};
 
-// his solution
-console.log(
-  movies
-    .filter((m) => m.rating > 4 && m.year === 2018)
-    .sort((a, b) => a.rating - b.rating)
-    .reverse()
-    .map((m) => m.title)
-);
+walk();
+run();
+runNamed();
+
+let move = run;
+move();
