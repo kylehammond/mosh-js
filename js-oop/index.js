ac24bd0309d9b13bc540d91023a1c7e86570d135
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-03 17:00:27 -0500

    abstraction (start)

--- js-oop/index.js
+++ js-oop/index.js
@@ -1,21 +1,14 @@
 function Circle(radius) {
   this.radius = radius;
+
+  this.defaultLocation = { x: 0, y: 0 };
+
+  this.computeOptimumLocation = function () {};
+
   this.draw = function () {
+    this.computeOptimumLocation();
     console.log("draw");
   };
 }
 
 const circle = new Circle(10);
-
-for (let key in circle) {
-  console.log(key); // radius draw
-}
-
-for (let key in circle) {
-  if (typeof circle[key] !== "function") console.log(key, circle[key]); // only radius
-}
-
-const keys = Object.keys(circle);
-console.log(keys); // array ["radius","draw"]
-
-if ("radius" in circle) console.log("Circle has a radius"); // circle has..
