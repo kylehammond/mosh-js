598ce091f2b6dbf341f15ac990f31dabdefdfee5
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-03 10:37:01 -0500

    Arrays 23 - Exercise 7 - Movies

--- index.js
+++ index.js
@@ -1,48 +1,30 @@
-// getMax(numbers) - give array and return largest number in array
-// implement without and with reduce()
-const numbers = [1, 2, 3, 4, 1, 8, 2, 0, -4];
+// write code to get all the movies in 2018 with rating > 4
+// sort by their rating in desc order
+// only pick the title property and display it on the console:
+// expected result:
+// 'b'
+// 'a'
 
-// my solution without reduce
-function getMax(array) {
-  return Math.max(...numbers);
-}
+const movies = [
+  { title: "a", year: 2018, rating: 4.5 },
+  { title: "b", year: 2018, rating: 4.7 },
+  { title: "c", year: 2018, rating: 3 },
+  { title: "d", year: 2017, rating: 4.5 },
+];
 
-// my solution with reduce
-function getMaxWithReduce(array) {
-  return array.reduce((accumulator, next) => {
-    if (next > accumulator) accumulator = next;
-    return accumulator;
-  });
-}
-
-let max = [];
-max = getMax(numbers);
-console.log(max);
-
-max = getMaxWithReduce(numbers);
-console.log(max);
+// my solution
+console.log(
+  movies
+    .filter((m) => m.rating > 4 && m.year === 2018)
+    .reverse((m) => m.rating) // apparently needed comparison
+    .map((m) => m.title)
+);
 
 // his solution
-function getMax(array) {
-  if (array.length === 0) return undefined;
-
-  let max = array[0];
-  for (let i = 1; i < array.length; i++) {
-    if (array[i] > max) max = array[i];
-  }
-  return max;
-}
-
-// his solution with reduce
-function getMaxWithReduce(array) {
-  if (array.length === 0) return undefined;
-
-  return array.reduce((a, b) => (a > b ? a : b));
-}
-
-let max = [];
-max = getMax(numbers);
-console.log(max);
-
-max = getMaxWithReduce(numbers);
-console.log(max);
+console.log(
+  movies
+    .filter((m) => m.rating > 4 && m.year === 2018)
+    .sort((a, b) => a.rating - b.rating)
+    .reverse()
+    .map((m) => m.title)
+);
