f417c2cc4c820f9eed623b51727f75c67f58a044
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-03 17:08:22 -0500

    private properties and methods

--- js-oop/index.js
+++ js-oop/index.js
@@ -1,14 +1,18 @@
 function Circle(radius) {
   this.radius = radius;
 
-  this.defaultLocation = { x: 0, y: 0 };
+  let defaultLocation = { x: 0, y: 0 };
 
-  this.computeOptimumLocation = function () {};
+  let computeOptimumLocation = function (factor) {};
 
   this.draw = function () {
-    this.computeOptimumLocation();
+    computeOptimumLocation(0.1);
+
     console.log("draw");
   };
 }
 
 const circle = new Circle(10);
+
+// closure:  when a function is in a function - it can access items in the inner function and its parent function
+// closure is of the child function - things in closure (only the parent things) will stay in memory unlike the scope (child things)
