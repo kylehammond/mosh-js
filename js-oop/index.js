0d3e032dc3ad84dc4107bf6916f0c6586d7c10fc
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-03 16:30:23 -0500

    constructors

--- js-oop/index.js
+++ js-oop/index.js
@@ -1,11 +1,9 @@
-function createCircle(radius) {
-  return {
-    radius,
-    draw: function () {
-      console.log("draw");
-    },
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function () {
+    console.log("draw");
   };
 }
 
-const circle = createCircle(1);
+const circle = new Circle(1);
 circle.draw();
