fc700f43ad6abf83a6a8915e8e48fc33f207ae8b
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-03 10:44:11 -0500

    hoisting

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 // Function Declaration
+walk(); // can call before defined
+
 function walk() {
   console.log("walk");
-}
+} // will be HOISTED by the javascript engine -
+// will be moved to top of script
 
-// Anonymous Function Expression
+// Function Expression
+run(); // can NOT call before defined - Uncaught ReferenceError: can't access lexical declaration 'run' before initialization
+// just like how you can't use a var before its defined
+
 let run = function () {
   console.log("run");
 };
-
-// Named Function Expression
-let runNamed = function runNamed() {
-  console.log("run named");
-};
-
-walk();
-run();
-runNamed();
-
-let move = run;
-move();
