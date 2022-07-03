3622632f93ffba7c44151736c09047d9e9fac9db
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-03 10:29:31 -0500

    Arrays 22 - Exercise 6 - Get max

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
-// make function countOccurrences(numbers,1) and count how many times that value exists
-// first without reduce
-// then with reduce
-
+// getMax(numbers) - give array and return largest number in array
+// implement without and with reduce()
 const numbers = [1, 2, 3, 4, 1, 8, 2, 0, -4];
 
 // my solution without reduce
-// function countOccurences(array, searchElement) {
-//   let count = 0;
-//   for (let element of array) {
-//     if (element === searchElement) count++;
-//   }
-//   return count;
-// }
+function getMax(array) {
+  return Math.max(...numbers);
+}
 
 // my solution with reduce
-// function countOccurences(array, searchElement) {
-//   const count = array.reduce((previousValue, currentValue) => {
-//     if (currentValue === searchElement) previousValue++;
-//     return previousValue;
-//   }, 0);
-//   return count;
-// }
+function getMaxWithReduce(array) {
+  return array.reduce((accumulator, next) => {
+    if (next > accumulator) accumulator = next;
+    return accumulator;
+  });
+}
 
-// let count = [];
-// count = countOccurences(numbers, 1);
-// console.log(count);
-// count = countOccurences(numbers, -1);
-// console.log(count);
-// count = countOccurences(numbers, -4);
-// console.log(count);
+let max = [];
+max = getMax(numbers);
+console.log(max);
 
-// his solution without reduce
-// same
+max = getMaxWithReduce(numbers);
+console.log(max);
+
+// his solution
+function getMax(array) {
+  if (array.length === 0) return undefined;
+
+  let max = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > max) max = array[i];
+  }
+  return max;
+}
 
 // his solution with reduce
-// function countOccurences(array, searchElement) {
-//   return array.reduce((accumulator, current) => {
-//     const occurence = current === searchElement ? 1 : 0;
-//     return accumulator + occurence;
-//   }, 0);
-// }
+function getMaxWithReduce(array) {
+  if (array.length === 0) return undefined;
 
-// let count = [];
-// count = countOccurences(numbers, 1);
-// console.log(count);
-// count = countOccurences(numbers, -1);
-// console.log(count);
+  return array.reduce((a, b) => (a > b ? a : b));
+}
+
+let max = [];
+max = getMax(numbers);
+console.log(max);
+
+max = getMaxWithReduce(numbers);
+console.log(max);
