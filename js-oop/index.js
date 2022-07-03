27b7f284f02e226d266c4ddfd43e7fe0617cd9b9
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-03 16:36:32 -0500

    constructor property

--- js-oop/index.js
+++ js-oop/index.js
@@ -1,3 +1,17 @@
+// factory function
+function createCircle(radius) {
+  return {
+    radius,
+    draw: function () {
+      console.log("draw");
+    },
+  };
+}
+
+const factoryCircle = createCircle(1);
+factoryCircle.draw();
+
+// constructor function
 function Circle(radius) {
   this.radius = radius;
   this.draw = function () {
@@ -5,5 +19,10 @@ function Circle(radius) {
   };
 }
 
-const circle = new Circle(1);
-circle.draw();
+const constructorCircle = new Circle(1);
+constructorCircle.draw();
+
+console.log("factory constructor", factoryCircle.constructor);
+console.log("constructor constructor", constructorCircle.constructor);
+
+// every object has a constructor property - referencing the constructor that was used to create the object
