83f11b5dfb2641f7dbb315a4f68e904f9db18b6d
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-04 09:07:16 -0500

    prototypical inheritance

--- js-oop/index.js
+++ js-oop/index.js
@@ -1,92 +1,11 @@
-//design a stopwatch object
+// Prototype - think "parent"
 
-//duration: 0
-//reset()
-//start() // can't call if started
-//stop() // can't call if stopped
+//.. think 'toString()' belonging to all objects - C# Object
 
-// my solution
-// function Stopwatch() {
-//   let duration = 0;
-//   let startTime = "";
-//   let stopTime = "";
-//   let running = false;
+let x = {};
+let y = {};
+//both reference object base / prototype
 
-//   this.reset = function () {
-//     duration = 0;
-//     startTime = "";
-//     stopTime = "";
-//     running = false;
-//   };
-//   this.start = function () {
-//     if (running) throw new Error("Already running");
-//     startTime = Date.now();
-//     running = true;
-//   };
-//   this.stop = function () {
-//     if (!running) throw new Error("Not started");
-//     stopTime = Date.now();
-//     running = false;
-//   };
+console.log(Object.getPrototypeOf(x) === Object.getPrototypeOf(y)); // true
 
-//   Object.defineProperty(this, "duration", {
-//     get: function () {
-//       duration += (stopTime - startTime) / 1000;
-//       return duration;
-//     },
-//     set: function (value) {
-//       duration = value;
-//     },
-//   });
-//   Object.defineProperty(this, "startTime", {
-//     get: function () {
-//       return startTime;
-//     },
-//     set: function (value) {
-//       startTime = value;
-//     },
-//   });
-//   Object.defineProperty(this, "stopTime", {
-//     get: function () {
-//       return stopTime;
-//     },
-//     set: function (value) {
-//       stopTime = value;
-//     },
-//   });
-// }
-
-// const sw = new Stopwatch();
-
-// his solution
-
-function Stopwatch() {
-  let startTime,
-    endTime,
-    running,
-    duration = 0;
-
-  this.start = function () {
-    if (running) throw new Error("Stopwatch has already started.");
-    running = true;
-    startTime = new Date();
-  };
-  this.stop = function () {
-    if (!running) throw new Error("Stopwatch is not started.");
-    running = false;
-    endTime = new Date();
-
-    const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
-    duration += seconds;
-  };
-  this.reset = function () {
-    startTime = null;
-    endTime = null;
-    running = false;
-    duration = false;
-  };
-
-  Object.defineProperty(this, "duration", {
-    get: function () {},
-  });
-}
+// js engine walks up the prototype chain (child -> parent) until it finds the method you want
