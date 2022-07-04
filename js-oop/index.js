187612c1a13a9750154be38dd9f5da2a71464f8b
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-04 09:26:42 -0500

    prototype vs instance members

--- js-oop/index.js
+++ js-oop/index.js
@@ -1,15 +1,42 @@
-// Object.getPrototypeOf(myObj);
+// function Circle(radius) {
+//   this.radius = radius;
 
-// myObj.__proto__ (parent of myObj)
-// Constructor.prototype () -> parent of myObj from above
+//   this.draw(log("draw"));
+// }
+
+// const c1 = new Circle(1);
+// const c2 = new Circle(1);
+
+// console.log(c1);
+// console.log(c2);
+// // now there exists many copies of the same function draw
 
 function Circle(radius) {
+  // instance members
   this.radius = radius;
+  this.move = function () {
+    this.draw(); // js engine will find this on the prototype
+    console.log("move");
+  };
 }
 
-console.log(Circle.prototype);
+const c1 = new Circle(1);
+const c2 = new Circle(1);
 
-const circle = new Circle(1);
+//prototype members
+Circle.prototype.draw = function () {
+  // this.move();
+  console.log("draw");
+}; // this moves
 
-let obj = {};
-console.log(Object.prototype);
+Circle.prototype.toString = function () {
+  return "Circle with radius " + this.radius;
+};
+
+console.log(c1.toString()); //Circle with radius 1
+// console.log(c1.draw()); // move  draw (with move in draw())
+console.log(c1.move()); // draw  move (with draw in move())
+
+console.log(c1);
+console.log(c2);
+// now there no draw function on the c1 or c2, it's in the prototype
