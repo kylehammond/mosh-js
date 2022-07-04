b844564ca1928b7ae0fc6b4dc0fc16f85f26b721
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-04 10:34:35 -0500

    method overriding

--- js-oop/index.js
+++ js-oop/index.js
@@ -1,38 +1,25 @@
-function Shape(color) {
-  this.color = color;
-}
-
-Shape.prototype.duplicate = function () {
-  console.log("duplicate");
-};
-
-// intermediate function inheritance
 function extend(Child, Parent) {
   Child.prototype = Object.create(Parent.prototype);
   Child.prototype.constructor = Child;
 }
 
-function Circle(radius, color) {
-  Shape.call(this, color);
-  this.radius = radius;
-}
+function Shape() {}
 
-extend(Circle, Shape); // extend circle with shape
-
-Circle.prototype.draw = function () {
-  console.log("draw");
+Shape.prototype.duplicate = function () {
+  console.log("duplicate");
 };
 
-function Square(size) {
-  this.size = size;
-}
+function Circle() {}
 
-extend(Square, Shape); // extend circle with shape
+extend(Circle, Shape);
 
-const s = new Shape("green");
-const c = new Circle(1, "red");
-const sq = new Square(10);
+// must be AFTER superclass method
+Circle.prototype.duplicate = function () {
+  Shape.prototype.duplicate.call(this); // call superclass/base method first
 
-console.log(s);
+  console.log("duplicate circle");
+};
+
+const c = new Circle();
+
 console.log(c);
-console.log(sq);
