1de3072b8ef6333ef8882247daa041c92d2ab3c8
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-04 10:59:32 -0500

    mixins

--- js-oop/index.js
+++ js-oop/index.js
@@ -1,6 +1,38 @@
-// inheritance heirarchies are fragile
-// favor composition over inheritance
+function mixin(target, ...sources) {
+  Object.assign(target, ...sources); // spread, not rest
+}
 
-// instead of big animal/mammal/fish heirarchy ..
+const canEat = {
+  eat: function () {
+    this.hunger--;
+    console.log("eating");
+  },
+};
 
-// canWalk, canEat, canSwim .. then compose things of those
+const canWalk = {
+  walk: function () {
+    console.log("walking");
+  },
+};
+
+const canSwim = {
+  swim: function () {
+    console.log("swimming");
+  },
+};
+
+//mixins
+function Person() {}
+
+mixin(Person.prototype, canEat, canWalk); // copy props and objs from one to another
+
+const person = new Person();
+console.log(person.eat());
+console.log(person.walk());
+
+function Goldfish() {}
+mixin(Goldfish.prototype, canEat, canSwim);
+
+const goldfish = new Goldfish();
+console.log(goldfish.eat());
+console.log(goldfish.swim());
