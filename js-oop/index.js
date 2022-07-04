9ea0b2b3b778b00f2335ad69bec3164f6e118741
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-04 11:39:20 -0500

    Prototypical inheritance 11 - Exercise 1 - Prototypical Inheritance His solution

--- js-oop/index.js
+++ js-oop/index.js
@@ -15,6 +15,44 @@
 // do not use extend - if you set your prototype to the prototype of the parent you won't get the instance methods click (you would get focus though)
 
 // my solution
+// function HtmlElement() {
+//   this.click = function () {
+//     console.log("clicked");
+//   };
+// }
+
+// HtmlElement.prototype.focus = function () {
+//   console.log("focused");
+// };
+
+// function HtmlSelectElement(items) {
+//   this.items = items === undefined ? [] : items;
+//   this.addItem = function (item) {
+//     items.push(item);
+//   };
+//   this.removeItem = function (item) {
+//     items.splice(items.indexOf("2"), 1);
+//   };
+// }
+
+// HtmlSelectElement.prototype = new HtmlElement();
+
+// const e = new HtmlElement();
+// const se = new HtmlSelectElement(["1", "2"]);
+
+// e.click();
+// e.focus();
+
+// console.log(se.items);
+// se.addItem("3");
+// console.log(se.items);
+// se.removeItem("2");
+// console.log(se.items);
+// se.click();
+// se.focus();
+
+// his solution
+// almost the same!!
 function HtmlElement() {
   this.click = function () {
     console.log("clicked");
@@ -25,17 +63,18 @@ HtmlElement.prototype.focus = function () {
   console.log("focused");
 };
 
-function HtmlSelectElement(items) {
-  this.items = items === undefined ? [] : items;
+function HtmlSelectElement(items = []) {
+  this.items = items;
   this.addItem = function (item) {
-    items.push(item);
+    this.items.push(item);
   };
   this.removeItem = function (item) {
-    items.splice(items.indexOf("2"), 1);
+    this.items.splice(this.items.indexOf("2"), 1);
   };
 }
 
 HtmlSelectElement.prototype = new HtmlElement();
+HtmlSelectElement.prototype.constructor = HtmlSelectElement;
 
 const e = new HtmlElement();
 const se = new HtmlSelectElement(["1", "2"]);
