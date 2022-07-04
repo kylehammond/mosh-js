3f727cc089c4ed3b99fcac5dfa79a3d3a2c8f6b4
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-04 10:52:44 -0500

    when to use inheritance

--- js-oop/index.js
+++ js-oop/index.js
@@ -1,49 +1,6 @@
-function extend(Child, Parent) {
-  Child.prototype = Object.create(Parent.prototype);
-  Child.prototype.constructor = Child;
-}
+// inheritance heirarchies are fragile
+// favor composition over inheritance
 
-function Shape() {}
+// instead of big animal/mammal/fish heirarchy ..
 
-Shape.prototype.duplicate = function () {
-  console.log("duplicate");
-};
-
-function Circle() {}
-
-extend(Circle, Shape);
-
-// must be AFTER superclass method
-Circle.prototype.duplicate = function () {
-  console.log("duplicate circle");
-};
-
-function Square() {}
-extend(Square, Shape);
-
-Square.prototype.duplicate = function () {
-  console.log("duplicate square");
-};
-
-const shapes = [new Circle(), new Square()];
-
-for (let shape of shapes) shape.duplicate(); // will call the proper implementation depending on object type
-
-// // the old, non-polymorphism way
-// for (let shape of shapes)
-// {
-//   if (shape.type === 'circle'){
-//     duplicateCircle();
-//   }
-//   else if (shape.type === 'square'){
-//     duplicateSquare(); //standalone functions not part of object
-//   }
-//   else
-//     duplicateShape();
-// }
-
-// const c = new Circle();
-// const sq = new Square();
-
-// console.log(c);
-// console.log(sq);
+// canWalk, canEat, canSwim .. then compose things of those
