afcdf54975c2e32e775da06b3316bd50fb433613
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-09 11:33:02 -0500

    Modules intro

--- js-oop/index.js
+++ js-oop/index.js
@@ -1,76 +1,23 @@
-// implement a stack using es6 classes
-// a stack is a special kind of data structure like a physical stack or pipe
+const _radius = new WeakMap();
 
-// count prop on object
-// prototype methods
-// push - add top
-// pop - remove top
-// peek - return top of stack
-
-// push('a')
-// push('b')
-// push('c')
-
-//const stack = new Stack()
-
-// pop() "stack is empty" if empty
-// peek() "stack is empty" if empty
-
-// my solution
-
-// const _stack = new WeakMap();
-// class Stack {
-//   constructor() {
-//     _stack.set(this, []);
-//   }
-
-//   count = 0;
-
-//   push(obj) {
-//     let stack = _stack.get(this);
-//     stack.push(obj);
-//     this.count++;
-//   }
-//   pop() {
-//     let stack = _stack.get(this);
-//     if (stack.length <= 0) throw new Error("Stack is empty");
-//     stack.pop();
-//     this.count--;
-//   }
-//   peek() {
-//     let stack = _stack.get(this);
-//     if (stack.length <= 0) throw new Error("Stack is empty");
-//     return stack[stack.length - 1];
-//   }
-// }
-
-// const stack = new Stack();
-
-// his solution
-
-const _items = new WeakMap();
-class Stack {
-  constructor() {
-    _items.set(this, []);
+class Circle {
+  constructor(radius) {
+    _radius.set(this, radius);
   }
 
-  push(obj) {
-    let stack = _items.get(this).push(obj);
-  }
-  pop() {
-    const items = _items.get(this);
-    if (items.length === 0) throw new Error("Stack is empty");
-    items.pop();
-  }
-  peek() {
-    const items = _items.get(this);
-    if (items.length === 0) throw new Error("Stack is empty");
-    return items[items.length - 1];
-  }
-
-  get count() {
-    _items.get(this).length;
+  draw() {
+    console.log("Circle with radius " + _radius.get(this));
   }
 }
 
-const stack = new Stack();
+const c = new Circle(10);
+c.draw();
+console.log(_radius.get(c)); // can do because it's all in the same file
+
+// no modules in ES5..
+// most popular module formats used then
+// AMD - browser (legacy)
+// CommonJS - node.js
+// UMD - browser/node.js  (legacy)
+
+// ES6 Modules are native
