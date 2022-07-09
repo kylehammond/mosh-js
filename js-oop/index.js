573f9ede6a32278e5fe7ce7935e380f6b6507922
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-09 10:01:03 -0500

    ES6 classes

--- js-oop/index.js
+++ js-oop/index.js
@@ -1,121 +1,31 @@
-// extend htmlselectelement and implement a render() method
-// render an html select/option list
-// create an htmlimageelement that inherits from htmlelement that can be clicked,focused, and renders its own way
-// optionally pass/set a src ... 'http://'
-// render an html img ... img src="http://" />
+// old
 
-// render should return strings
+// function Circle(radius){
+//   this.radius = radius;
 
-// finally, create array of elements of htmlselectelement(1,2,3) and htmlImageElement('http://') and call render on each
-
-// my solution
-// function HtmlElement() {
-//   this.click = function () {
-//     console.log("clicked");
-//   };
+//   this.draw = function() {
+//     console.log('draw');
+//   }
 // }
 
-// HtmlElement.prototype.focus = function () {
-//   console.log("focused");
-// };
+// newer
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
 
-// function HtmlSelectElement(items = []) {
-//   this.items = items;
-//   this.addItem = function (item) {
-//     this.items.push(item);
-//   };
-//   this.removeItem = function (item) {
-//     this.items.splice(this.items.indexOf("2"), 1);
-//   };
-//   this.render = function () {
-//     const html = `
-// <select>
-//   ${this.items.map((i) => `<option>${i}</option>`).join(`
-//   `)}
-// </select>`;
-//     return html;
-//   };
-// }
+    this.move = function () {}; // object instance only
+  }
 
-// HtmlSelectElement.prototype = new HtmlElement();
-// HtmlSelectElement.prototype.constructor = HtmlSelectElement;
-
-// function HtmlImageElement(src) {
-//   this.src = src;
-//   this.render = function () {
-//     return `<img src='${src} />`;
-//   };
-// }
-
-// HtmlImageElement.prototype = new HtmlElement();
-// HtmlImageElement.prototype.constructor = HtmlImageElement;
-
-// const e = new HtmlElement();
-// const se = new HtmlSelectElement(["1", "2", "3"]);
-// const i = new HtmlImageElement("http://www.goobles.com");
-
-// const elements = [
-//   new HtmlSelectElement(["1", "2", "3"]),
-//   new HtmlImageElement("http://www.goobles.com"),
-// ];
-
-// for (let element of elements) {
-//   console.log(element.render());
-//   // element.click();
-//   // element.focus();
-// }
-
-// his solution
-function HtmlElement() {
-  this.click = function () {
-    console.log("clicked");
-  };
+  // part of prototype
+  draw() {
+    console.log("draw");
+  }
 }
 
-HtmlElement.prototype.focus = function () {
-  console.log("focused");
-};
+const c = new Circle(1);
+console.log(c.draw());
 
-function HtmlSelectElement(items = []) {
-  this.items = items;
-  this.addItem = function (item) {
-    this.items.push(item);
-  };
-  this.removeItem = function (item) {
-    this.items.splice(this.items.indexOf("2"), 1);
-  };
-  this.render = function () {
-    return `
-<select>
-  ${this.items.map((item) => `<option>${item}</option>`).join("")}
-</select>`;
-  };
-}
+console.log(typeof Circle); // function   - class is a type of function
 
-HtmlSelectElement.prototype = new HtmlElement();
-HtmlSelectElement.prototype.constructor = HtmlSelectElement;
-
-function HtmlImageElement(src) {
-  this.src = src;
-  this.render = function () {
-    return `<img src='${this.src} />`;
-  };
-}
-
-HtmlImageElement.prototype = new HtmlElement();
-HtmlImageElement.prototype.constructor = HtmlImageElement;
-
-const e = new HtmlElement();
-const se = new HtmlSelectElement(["1", "2", "3"]);
-const i = new HtmlImageElement("http://www.goobles.com");
-
-const elements = [
-  new HtmlSelectElement(["1", "2", "3"]),
-  new HtmlImageElement("http://www.goobles.com"),
-];
-
-for (let element of elements) {
-  console.log(element.render());
-  // element.click();
-  // element.focus();
-}
+//babeljs.io
+//use to convert new JS to old
