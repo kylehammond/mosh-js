b16efce55f7cc37babe5585c03904142af63a315
Kyle Hammond <kyle.hammond@gmail.com>
2022-07-09 10:45:40 -0500

    method overriding again

--- js-oop/index.js
+++ js-oop/index.js
@@ -1,30 +1,16 @@
 class Shape {
-  constructor(color) {
-    this.color = color;
-  }
-
   move() {
     console.log("move");
   }
 }
 
 class Circle extends Shape {
-  constructor(color, radius) {
-    super(color); // have to call or it will throw exception because you didn't call super's const
-    this.radius = radius;
-  }
-  draw() {
-    console.log("draw");
+  move() {
+    super.move(); // optional
+    console.log("move override");
   }
 }
 
-const c = new Circle("blue", 1);
+const c = new Circle();
 console.log(c);
-// Circle {}
-// [[Prototype]]: Shape
-// constructor: class Circle
-// draw: ƒ draw()
-// [[Prototype]]: Object
-
 c.move();
-c.draw();
